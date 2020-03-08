bd54ce70c18027939a0c50a4596cce8e2daefd24
Chris Young <christopher.young@utoronto.ca>
2020-03-08 15:09:25 -0400

    syntax changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import { withAuthenticator } from 'aws-amplify-react'
- import React, { useEffect } from 'react'
- import { Auth } from 'aws-amplify'
+import React, { useEffect } from 'react'
+import { Auth } from 'aws-amplify'
 
- function App() {
-   useEffect(() => {
-     Auth.currentAuthenticatedUser()
-       .then(user => console.log({ user }))
-       .catch(error => console.log({ error }))
-   })
-   return (
-     <div className="App">
-       <p>
-         Hello! This is an AWS Amplify application.
-       </p>
-     </div>
-   )
- }
+function App() {
+  useEffect(() => {
+    Auth.currentAuthenticatedUser()
+      .then(user => console.log({ user }))
+      .catch(error => console.log({ error }))
+  })
+ return (
+   <div className="App">
+     <p>
+       Hello! This is an AWS Amplify application.
+     </p>
+   </div>
+ )
+}
 
- export default withAuthenticator(App, { includeGreetings: true })
+export default withAuthenticator(App, { includeGreetings: true })
