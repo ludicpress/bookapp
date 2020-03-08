2a54909176bac2e490046b24a31f628791ce46a8
Chris Young <christopher.young@utoronto.ca>
2020-03-08 15:06:59 -0400

    configure the react application

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { withAuthenticator } from 'aws-amplify-react'
+ import React, { useEffect } from 'react'
+ import { Auth } from 'aws-amplify'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ function App() {
+   useEffect(() => {
+     Auth.currentAuthenticatedUser()
+       .then(user => console.log({ user }))
+       .catch(error => console.log({ error }))
+   })
+   return (
+     <div className="App">
+       <p>
+         Hello! This is an AWS Amplify application.
+       </p>
+     </div>
+   )
+ }
 
-export default App;
+ export default withAuthenticator(App, { includeGreetings: true })
